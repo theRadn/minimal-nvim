f8cb15402b205ada8fe9b55632387bec41dc9c22
theRadn <128877618+theRadn@users.noreply.github.com>
2025-05-05 14:25:52 +0700

    Add files via upload

---
+++ nvim/init.lua
@@ -0,0 +1,42 @@
+-- Ensure lazy is in runtime path
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  print("lazy.nvim not found, please install it as per instructions")
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Setup lazy.nvim and load plugins
+require("lazy").setup({
+  -- Plugins go here as tables
+  -- Example: Hop plugin
+  {
+    "phaazon/hop.nvim",
+    branch = "v2",
+    config = function()
+      require("hop").setup()
+    end
+  }
+})
+
+-- place this in one of your configuration file(s)
+local hop = require('hop')
+local directions = require('hop.hint').HintDirection
+vim.keymap.set('', 'f', function()
+  hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = false })
+end, {remap=true})
+vim.keymap.set('', 'F', function()
+  hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = false })
+end, {remap=true})
+vim.keymap.set('', 't', function()
+  hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
+end, {remap=true})
+vim.keymap.set('', 'T', function()
+  hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
+end, {remap=true})
+
+vim.wo.number = true        -- Enable absolute line numbers
+
+vim.opt.tabstop = 4        -- Number of visual spaces per TAB
+vim.opt.shiftwidth = 4     -- Number of spaces to use for each step of (auto)indent
+vim.opt.softtabstop = 4    -- Number of spaces a <Tab> counts for while editing
+vim.opt.expandtab = true    -- Use spaces instead of tabs
